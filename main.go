6aae55e298d1fef09001ee14932ab2728da57a0a
Hayoung Kang <monsun69@protonmail.ch>
2018-10-12 08:42:45 -0700

    설치 서비스 스크립트 추가

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 var baseUrl = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-var commutateDelay = 300
+var commutateDelay = time.Duration(300)
 
 var c client.Instance
 var s models.Main
